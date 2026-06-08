14c545d31832263bb5cd2c87dd4966c6bd394dcb
abhijeetsp98_vps <abhijeetsp98@gmail.com>
2026-06-08 08:18:41 +0000

    VPS changes

--- frontend/src/components/constant.js
+++ frontend/src/components/constant.js
@@ -1,4 +1,4 @@
-export const BACKEND_API_BASE_URL = 'https://vishwaspower.onrender.com';
+export const BACKEND_API_BASE_URL = 'https://vishwaspower.in';
 export const BACKEND_IMG_API_BASE_URL = 'https://res.cloudinary.com/dfqczollw/image/upload/';
 
 // export const BACKEND_API_BASE_URL = 'http://localhost:8000';
