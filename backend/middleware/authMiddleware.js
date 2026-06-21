20c92ac0acd1887a0ad6a8472d183e2bcc899905
Abhijeet Padghan <apadghan@qti.qualcomm.com>
2026-06-21 14:40:33 +0530

    Fix for authentication

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -7,7 +7,7 @@ export const protect = async (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = await User.findById(decoded.id).select('-password');
+    req.user = await User.findOne({ _id: decoded.id }).select('-password');
     next();
   } catch (err) {
     res.status(401).json({ message: 'Not authorized, token failed' });
